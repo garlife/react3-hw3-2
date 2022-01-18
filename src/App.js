2d5eecfeda4fd22f38c0b5db39a2c4381063de17
Романов Александр Владимирович <garlife@gmail.com>
2022-01-18 23:39:34 +0300

    display flex added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,100 @@
-import './App.css';
-import { Column, Table, Cell } from "@blueprintjs/table";
+import "./App.css";
+import React from "react";
+import Menu from "./Menu.js";
+import Footer from "./Footer.js";
 
-
-const dollarCellRenderer = () => (
-  <Cell>Пока</Cell>
-);
-const euroCellRenderer = () => (
-  <Cell>Привет</Cell>
-);
 function App() {
   return (
-<Table numRows={2}>
-    <Column cellRenderer={dollarCellRenderer} />
-    <Column cellRenderer={euroCellRenderer}/>
-</Table>
+    <div
+      className="container"
+      style={{
+        padding: "10px",
+        display: "flex",
+        width: "100%",
+        flexDirection: "column",
+      }}
+    >
+      <div
+        className="header"
+        style={{
+          height: "100px",
+          width: "100%",
+          border: "1px solid",
+          display: "inline-flex",
+          justifyContent: "space-between",
+          height: "100px",
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            border: "1px solid",
+            width: "20%",
+            alignSelf: "stretch",
+          }}
+        >
+          <Menu mass1="mass1" />
+        </div>
+        <div
+          style={{
+            border: "1px solid",
+            width: "20%",
+            alignSelf: "stretch",
+          }}
+        >
+          <Menu mass2="mass2" />
+        </div>
+      </div>
+      <div
+        className="content"
+        style={{
+          border: "1px solid",
+          height: "400px",
+          width: "100%",
+          display: "inline-flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <div
+          style={{
+            width: "30%",
+            border: "1px solid",
+            alignSelf: "stretch",
+            width: "400px",
+          }}
+        >
+          <nav>nav</nav>
+        </div>
+        <div
+          style={{
+            width: "40%",
+            border: "1px solid",
+            alignSelf: "stretch",
+          }}
+        >
+          <section>section</section>
+        </div>
+        <div
+          style={{
+            width: "30%",
+            border: "1px solid",
+            alignSelf: "stretch",
+          }}
+        >
+          <aside>aside</aside>
+        </div>
+      </div>
+      <div
+        style={{
+          height: "100px",
+          width: "100%",
+          border: "1px solid",
+          alignSelf: "flex-end"
+        }}
+      >
+        <Footer footer="footer" />
+      </div>
+    </div>
   );
 }
 
---
+++ src/Menu.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+const Menu = (props) => (
+  <div>
+    <div>
+      <div 
+    //   style={{justifyContent: "left"}}
+      >{props.mass1}</div>
+      <div 
+    //   style={{justifyContent: "right"}}
+      >{props.mass2}</div>
+    </div>
+  </div>
+);
+
+export default Menu;
